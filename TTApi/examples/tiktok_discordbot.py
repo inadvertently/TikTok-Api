3f4ecba67eb0e623cf2e50435579653a21d56e07
sorrow <71972762+inadvertently@users.noreply.github.com>
2022-09-05 16:34:33 -0400

    Async fyp support
    For those using later versions of dpy such as, 2.0+ you're going to want to await the function as this API depends on requests.

--- TTApi/examples/tiktok_discordbot.py
+++ TTApi/examples/tiktok_discordbot.py
@@ -3,7 +3,6 @@ from TTApi import TikTokApi
 from io import BytesIO
 import discord
 
-
 tiktok = TikTokApi()
 bot_token = ""
 prefix = ";"
@@ -30,7 +29,18 @@ class tiktokbot(discord.Client):
             else:
                 await msg.channel.send(f'Correct usage: `;tiktok <videoID | videoURL>`')
 
-
-
+    async def fyp(self, ctx):
+        fyp_videos = tiktok.feed.for_you()
+        no_watermark_download = fyp_videos[0]["download_urls"]["no_watermark"]
+        video_binary = tiktok.video.get_video_binary(no_watermark_download)
+        bytes_io = BytesIO(video_binary)
+        embed = discord.Embed(color =0x000000)
+        embed.set_author(name=f"{fyp_videos[0]['author']['username']}・{fyp_videos[0]['author']['user_id']}", icon_url="https://cdn.discordapp.com/emojis/1010602768660181012.png?size=256", url=fyp_videos[0]['video_url'])
+        embed.description = f'**{fyp_videos[0]["description"]}**'
+        #embed.set_author(name="Tiktok by @"+video_data["author"]["username"], icon_url="https://cdn.discordapp.com/emojis/1010602768660181012.png?size=256", url=video_data["video_url"])
+        embed.set_footer(text=f"💬 {fyp_videos[0]['stats']['comment_count']} | 👍 {fyp_videos[0]['stats']['likes']} | 🔗 {fyp_videos[0]['stats']['shares']} ({fyp_videos[0]['stats']['views']} views)\n🎵 {fyp_videos[0]['music']['title']} (by {fyp_videos[0]['music']['author']})")
+        await ctx.reply(file=discord.File(fp=bytes_io, filename="tiktok.mp4"), embed=embed)
+                
+                
 client = tiktokbot()
 client.run(bot_token, bot=True)
